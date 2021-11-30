8dda2668b7e0dbde5029ae70939162868cfe08d6
jzdziebko <93586501+jzdziebko@users.noreply.github.com>
2021-11-30 01:33:45 +0100

    Update jsa i cssa

--- main.js
+++ main.js
@@ -4,15 +4,21 @@ var globKon = 0;
 var globInt = 0;
 var globWie = 0;
 var globCha = 0;
-const prof = 2;
+var globSilS = 0;
+var globZreS = 0;
+var globKonS = 0;
+var globIntS = 0;
+var globWieS = 0;
+var globChaS = 0;
 function updateSil(x) 
 {
     const input = document.getElementById('cstr')
     const output1 = document.getElementById('modS');
     const output2 = document.getElementById('saveS');
     let y=Math.floor((input.value-10+globSil)/2);
+    let z=y+globSilS
     output1.textContent = y;
-    output2.textContent = y;
+    output2.textContent = z;
 }
 function updateZre(x) 
 {
@@ -20,8 +26,9 @@ function updateZre(x)
     const output1 = document.getElementById('modZ');
     const output2 = document.getElementById('saveZ');
     let y=Math.floor((input.value-10+globZre)/2);
+    let z=y+globZreS;
     output1.textContent = y;
-    output2.textContent = y;
+    output2.textContent = z;
 }
 function updateKon(x) 
 {
@@ -29,8 +36,9 @@ function updateKon(x)
     const output1 = document.getElementById('modK');
     const output2 = document.getElementById('saveK');
     let y=Math.floor((input.value-10+globKon)/2);
+    let z=y+globKonS;
     output1.textContent = y;
-    output2.textContent = y;
+    output2.textContent = z;
 }
 function updateInt(x) 
 {
@@ -38,8 +46,9 @@ function updateInt(x)
     const output1 = document.getElementById('modI');
     const output2 = document.getElementById('saveI');
     let y=Math.floor((input.value-10+globInt)/2);
+    let z=y+globIntS;
     output1.textContent = y;
-    output2.textContent = y;
+    output2.textContent = z;
 }
 function updateWie(x) 
 {
@@ -47,8 +56,9 @@ function updateWie(x)
     const output1 = document.getElementById('modM');
     const output2 = document.getElementById('saveM');
     let y=Math.floor((input.value-10+globWie)/2);
+    let z=y+globWieS;
     output1.textContent = y;
-    output2.textContent = y;
+    output2.textContent = z;
 }
 function updateCha(x) 
 {
@@ -56,8 +66,9 @@ function updateCha(x)
     const output1 = document.getElementById('modC');
     const output2 = document.getElementById('saveC');
     let y=Math.floor((input.value-10+globCha)/2);
+    let z=y+globChaS;
     output1.textContent = y;
-    output2.textContent = y;
+    output2.textContent = z;
 }
 function updateD()
 {
@@ -121,5 +132,61 @@ function updateC()
 }
 function updateCl()
 {
-
+    globSilS = 0;
+    globZreS = 0;
+    globKonS = 0;
+    globIntS = 0;
+    globWieS = 2;
+    globChaS = 2;
+    updateSil();
+    updateZre();
+    updateKon();
+    updateInt();
+    updateWie();
+    updateCha();
+}
+function updateFi()
+{
+    globSilS = 2;
+    globZreS = 0;
+    globKonS = 2;
+    globIntS = 0;
+    globWieS = 0;
+    globChaS = 0;
+    updateSil();
+    updateZre();
+    updateKon();
+    updateInt();
+    updateWie();
+    updateCha();
+}
+function updateRo()
+{
+    globSilS = 0;
+    globZreS = 2;
+    globKonS = 0;
+    globIntS = 2;
+    globWieS = 0;
+    globChaS = 0;
+    updateSil();
+    updateZre();
+    updateKon();
+    updateInt();
+    updateWie();
+    updateCha();
+}
+function updateWi()
+{
+    globSilS = 0;
+    globZreS = 0;
+    globKonS = 0;
+    globIntS = 2;
+    globWieS = 2;
+    globChaS = 0;
+    updateSil();
+    updateZre();
+    updateKon();
+    updateInt();
+    updateWie();
+    updateCha();
 }
